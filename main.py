ecc6189965dbaa617c6ea5fe63a92ff07792415c
Shoaib Hossain <shoaibhossain24@gmail.com>
2025-02-14 03:28:58 +0600

    Day 2 first

---
+++ main.py
@@ -0,0 +1,5 @@
+two_digit_number=input("Enter a two digit number to get its summation : \n");
+
+sum_two_digit_number = sum( [int(two_digit_number[0]) , int(two_digit_number[1])]);
+
+print("The sum of the two digit number is : " + str(sum_two_digit_number));
