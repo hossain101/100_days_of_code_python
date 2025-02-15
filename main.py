7243c8fca7ee46286a27bb24c18e99b66d4662c6
Shoaib Hossain <shoaibhossain24@gmail.com>
2025-02-16 02:22:54 +0600

    Final commit to master

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-# Tip Calculator.
-
-print("Welcome to the tip calculator.")
-total_bill = float(input("What was the total bill ? \n"))
-percentage_tip = int(input("What percentage of tip would you like to give? 10, 12, 0r 15? \n"))
-people_split = int(input("How many people to split the bill? \n"))
-
-split_bill = total_bill * (1+ percentage_tip/100) /people_split
-
-print(f"Each person should pay : ${round(split_bill,2)}")
-
+#Keeping the master branch empty and will create new branches as I progress through the course.
