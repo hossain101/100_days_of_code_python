17131ccbd664146e4ed2ec46ae97b81fa71dd562
Shoaib Hossain <shoaibhossain24@gmail.com>
2025-02-17 01:15:46 +0600

    Ticket Counter : Nested if elif else statement

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-# Odd or Even
+# Ticket Counter : Nested if elif else statement
 
-_input = int(input("Enter a number : "))
+print("Welcome to the rollercoaster ride!!")
+height = int(input("Enter your height in cm : "))
+age = int(input("Enter your age in years : "))
 
-if _input%2==0:
-    print("This is an even number.")
+if height >= 120:
+    print("You are tall enough to ride the rollercoaster!!")
+    if age >= 18:
+        print("Your bill is $20")
+    elif age >=12:
+        print("Your bill is $15")
+    else:
+        print("Your bill is $10")
 else:
-    print("This is an odd number.")
+    print("Get some inches in you before you can have a ride ")
