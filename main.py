052a84d86bf3d8899f37bc0eaee571fca4ffcc4b
Shoaib Hossain <shoaibhossain24@gmail.com>
2025-02-18 00:03:57 +0600

    # Love calculator

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
-# Pizza Ordering system
+# Love calculator
 
-print("Welcome to Python Pizza Deliveries!")
+print("Welcome to the love calculator ")
 
-size = input("What size pizza do you want? S,M,L : ").capitalize()
+name1 = input("What is your name? : ").lower()
+name2 = input("What is your lover's name : ").lower()
 
-add_pepperoni = input("Do you want pepperoni?(Y/N) : ")
-extra_cheese = input("Do you want extra cheese?(Y/N) : ")
+count_t = name1.count("t")
+count_r = name1.count("r")
+count_u = name1.count("u")
+count_e = name1.count("e")
 
-total_bill = 0
+count_t += name2.count("t")
+count_r += name2.count("r")
+count_u += name2.count("u")
+count_e += name2.count("e")
 
-if size == "S":
-    total_bill += 15
+count_l = name1.count("l")
+count_o = name1.count("o")
+count_v = name1.count("v")
+count_e = name1.count("e")
 
-elif size == "M":
-    total_bill += 20
-elif size == "L":
-    total_bill += 25
+count_l += name2.count("l")
+count_o += name2.count("o")
+count_v += name2.count("v")
+count_e += name2.count("e")
 
-if add_pepperoni == "Y":
-    if size == "S":
-        total_bill += 2
+score_true = count_t + count_r + count_u + count_e
+score_love = count_l + count_o + count_v + count_e
 
-    else:
-        total_bill += 3
+print(f"Your Score : {score_true}{score_love}")
 
+if (
+    int(str(score_true) + str(score_love)) < 10
+    or int(str(score_true) + str(score_love)) > 90
+):
+    print("You go together like coke and mentos.")
 
-if extra_cheese == "Y":
-    total_bill += 1
+elif (
+    int(str(score_true) + str(score_love)) > 40
+    and int(str(score_true) + str(score_love)) < 50
+):
+    print("You are alright together!")
 
+else:
 
-print(f"Your Total Bill is : ${total_bill}")
+    print(f"Your Score : {score_true}{score_love}")
