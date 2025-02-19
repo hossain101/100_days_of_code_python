681ca632f5a7895f1c2b8677b58ed2aff3c96603
Shoaib Hossain <shoaibhossain24@gmail.com>
2025-02-20 03:37:12 +0600

    # Bill Split game

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# Bill Split game
 import random
 
-random_side = random.randint(0, 1)
+name_string = input("Please enter names of participants, separated by , and a space.\n")
 
-if random_side == 1:
-    print("Heads")
-else:
-    print("Tails")
+name_list = name_string.split(", ")
+
+random_index = random.randint(0, len(name_list) - 1)
+
+print(f"{name_list[random_index]} will have to pay the bill.")
